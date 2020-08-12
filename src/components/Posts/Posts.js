714220d88d9055f8eba88d0befa99659f854f3ed
Andrew Ruiz <nukedoom@gmail.com>
2020-08-11 19:40:17 -0700

    Completed project

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,11 +5,22 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  console.log(posts, `here`)
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+
+      {
+        posts.map(elem => {
+          console.log(elem)
+          return <Post
+          key={elem.id}
+          post={elem}
+          likePost={likePost}
+          />
+        })
+      }
     </div>
   );
 };
